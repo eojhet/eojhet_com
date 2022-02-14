41be7cee26f74103463d76261a29784319df21b5
eojhet <eojhet@gmail.com>
2022-02-14 15:38:49 -0500

    Created landing page

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import styles from "./navbar.module.scss";
 import ReactModal from 'react-modal';
 import { useState } from "react";
+import Head from "next/head";
 
 export default function Navbar () {
   const [menu, setMenu] = useState(false);
@@ -12,53 +13,64 @@ export default function Navbar () {
   }
 
   return (
-    <nav className={styles.nav}>
-      <div className={styles.container}>
-        <h3><Link href="/"><a>EOJHET.COM</a></Link></h3>
-        <div className={styles.hamburger} onClick={handleHamburger}>
-          <span className={styles.top}></span>
-          <span className={styles.middle}></span>
-          <span className={styles.bottom}></span>
-        </div>
-        <ReactModal
-          isOpen={menu}
-          onRequestClose={handleHamburger}
-          ariaHideApp={false}
-          contentLabel="Menu Options"
-          style={{
-            overlay: {
-              position: 'fixed',
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              backgroundColor: 'rgba(255, 255, 255, 0.1)'
-            },
-            content: {
-              position: 'fixed',
-              top: '3.2rem',
-              left: '0',
-              right: '0',
-              bottom: '0',
-              border: '1px solid #ccc',
-              background: '#fff',
-              overflow: 'auto',
-              WebkitOverflowScrolling: 'touch',
-              borderRadius: '4px',
-              outline: 'none',
-              padding: '20px',
-              textAlign: 'right',
-            }
-          }}       
-        >
-          <div>
-            <p>Asses Exploding</p>
-            <p><Link href="/"><a onClick={handleHamburger}>Home</a></Link></p>
-            <p><Link href="/cuttingslab"><a onClick={handleHamburger}>The Cutting Slab</a></Link></p>
-            <p>GOATS</p>
+    <>
+      <Head>
+        <title>Eojhet</title>
+        <meta name="Eojhet" content="I am Joe" />
+        <link rel="icon" href="favico.ico?v=4" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" /> 
+      </Head>
+      
+      <nav className={styles.nav}>
+        <div className={styles.container}>
+          <h3><Link href="/"><a>EOJHET.COM</a></Link></h3>
+          <div className={styles.hamburger} onClick={handleHamburger}>
+            <span className={styles.top}></span>
+            <span className={styles.middle}></span>
+            <span className={styles.bottom}></span>
           </div>
-        </ReactModal>
-      </div>
-    </nav>
+          <ReactModal
+            isOpen={menu}
+            onRequestClose={handleHamburger}
+            ariaHideApp={false}
+            contentLabel="Menu Options"
+            style={{
+              overlay: {
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundColor: 'rgba(255, 255, 255, 0.1)'
+              },
+              content: {
+                position: 'fixed',
+                top: '3.2rem',
+                left: '0',
+                right: '0',
+                bottom: '0',
+                border: '1px solid #ccc',
+                background: '#fff',
+                overflow: 'auto',
+                WebkitOverflowScrolling: 'touch',
+                borderRadius: '4px',
+                outline: 'none',
+                padding: '20px',
+                textAlign: 'right',
+              }
+            }}       
+          >
+            <div>
+              <p>Asses Exploding</p>
+              <p><Link href="/"><a onClick={handleHamburger}>Home</a></Link></p>
+              <p><Link href="/cuttingslab"><a onClick={handleHamburger}>The Cutting Slab</a></Link></p>
+              <p>GOATS</p>
+            </div>
+          </ReactModal>
+        </div>
+      </nav>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./index.module.scss";
 
 export default function Home () {
@@ -6,19 +7,69 @@ export default function Home () {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        Welcome to Eojhet...
-        <div className={styles.title}>
-          <h1>GREETINGS FROM JOE...</h1>
+      
+        <div className={styles.left}>
+          <h2>About Me:</h2>
+          <div className={styles.leftContent}>
+            <p>My name is Joe Gonzalez.</p>
+            <p>I am a web developer out of Norfolk, Virginia.</p>
+            <p>I love working with JavaScript, HTML, CSS, React, Nodejs and much more!</p>
+            <p>Please feel free to <a href="mailto:eojhet@gmail.com"><strong>Contact Me</strong></a> for questions, comments, or if you are hiring.</p>
+            <p>Thank you so much for visiting!</p>
+          </div>
         </div>
-        <div>
-          <h1><Link href="./cuttingslab"><a>The Cutting Slab</a></Link></h1>
+
+        <div className={styles.center}>
+          <div className={styles.title}>
+            <h1>"CONTENT"...</h1>
+          </div>
+          <Link href="./cuttingslab"><div className={styles.card}>
+            <div className={styles.cardDescription}>
+              <h2>The Cutting Slab</h2>
+              <p>{"Come have a taste of your favorite bread at the home The Cutting Slab. We will cut your bread to perfection!"}</p>
+            </div>
+            <div className={styles.cardPicture}>
+              <Image
+                src="/images/jumpingknifejack.jpg"
+                alt="The Amazing Jumping Knife Jack"
+                width={1282}
+                height={1920}
+              />
+            </div>
+          </div></Link>
+          <Link href="./cuttingslab"><div className={styles.card}>
+            <div className={styles.cardDescription}>
+              <h2>The Cutting Slab</h2>
+              <p>{"Come have a taste of your favorite bread at the home The Cutting Slab. We will cut your bread to perfection!"}</p>
+            </div>
+            <div className={styles.cardPicture}>
+              <Image
+                src="/images/jumpingknifejack.jpg"
+                alt="The Amazing Jumping Knife Jack"
+                width={1282}
+                height={1920}
+              />
+            </div>
+          </div></Link>
         </div>
-        <div className={styles.sunContainer}>
+
+        <div className={styles.right}>
+          <h2>Thank you for visiting!</h2>
+          <div className={styles.rightContent}>
+            <p>Catch me on all the things:</p>
+            <p>Twitter: <a href="https://twitter.com/eojhet" target="_blank">@Eojhet</a></p>
+            <p>GitHub: <a href="https://github.com/eojhet" target="_blank">GitHub.com/eojhet</a></p>
+            <p><a href="https://www.linkedin.com/in/joseph-gonzalez-0844b947/" target="_blank">LinkedIn</a></p>
+          </div>
+        </div>
+
+        
+        {/* <div className={styles.sunContainer}>
           <img className={styles.sun} src="/home/vaporSun.svg" alt="Vapor Sun" />
         </div>
         <div className={styles.triangleContainer}>
           <img className={styles.triangle} src="/home/vaporTrianglesOpt.svg" />
-        </div>
+        </div> */}
       </div>
     </main>
   );

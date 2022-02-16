66361b2360c5bb7ac1e4bc653531f936121cfe5f
eojhet <eojhet@gmail.com>
2022-02-16 14:59:29 -0500

    fixed build errors from using apostrophes

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -6,11 +6,11 @@ export default function Footer () {
   return (
     <footer className={styles.footer}>
       <div className={styles.socials}>
-        <a href="https://twitter.com/eojhet" target="_blank"><img src="/home/twitter1.svg" alt="twitter" width="35px"/></a>
+        <a href="https://twitter.com/eojhet" target="_blank" rel="noreferrer"><img src="/home/twitter1.svg" alt="twitter" width="35px"/></a>
         &nbsp;&nbsp;&nbsp;
-        <a href="https://github.com/eojhet" target="_blank"><img src="/home/GitHubDark.svg" alt="github" width="35px"/></a>
+        <a href="https://github.com/eojhet" target="_blank" rel="noreferrer"><img src="/home/GitHubDark.svg" alt="github" width="35px"/></a>
         &nbsp;&nbsp;&nbsp;
-        <a href="https://www.linkedin.com/in/joseph-gonzalez-0844b947/" target="_blank"><img src="/home/linkedin.png" alt="linkedin" width="35px"/></a>
+        <a href="https://www.linkedin.com/in/joseph-gonzalez-0844b947/" target="_blank" rel="noreferrer"><img src="/home/linkedin.png" alt="linkedin" width="35px"/></a>
       </div>
       <div className={styles.copyright}>
         <Link href="/">&copy;eojhet 2022</Link>

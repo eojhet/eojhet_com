7c40ebd038b3fdfc33f31e1dc6e7e327b00b60cb
Joe Gonzalez <eojhet@gmail.com>
2022-02-20 21:33:32 -0500

    changed landing from 3 to 2 column - cuttingboard changed a lot

--- pages/cuttingslab/index.js
+++ pages/cuttingslab/index.js
@@ -1,7 +1,6 @@
 // The Cutting Slab
 
 import styles from './index.module.scss';
-import Image from 'next/image';
 import Head from 'next/head';
 
 
@@ -11,34 +10,30 @@ export default function Home() {
       <Head>
         <title>The Cutting Slab</title>
         <meta name="The Cutting Slab" content="We Cut Bread" />
-        <link rel="icon" href="favico.ico?v=4" />
       </Head>
       
       <div className={styles.banner}>
-        <div className={styles.logo}>
-          <Image
-            src="/images/thecuttingslabsm.png"
-            alt="The Cutting Slab"
-            width={175}
-            height={60}
-          />        
+        <div className={styles.bannerElements}>
+            <img
+              src="/images/thecuttingslabsm.svg"
+              alt="The Cutting Slab"
+              className={styles.svgLogo}
+            />        
+          <div className={styles.title}>
+            <p>FINE BREAD CUTTERY</p>
+          </div>
         </div>
-        <div className={styles.title}>
-          <p>Fun for everyone!</p>
+        <div className={styles.storefront}>
+            <img
+              src="/images/storefront.jpg"
+              alt="The Cutting Slab Store Front"
+              className={styles.storefrontPic}
+            />
         </div>
       </div>
       
       <div className={styles.container}>
-        <div className={styles.storefront}>
-          <div className={styles.image}>
-            <Image
-              src="/images/storefront.jpg"
-              alt="The Cutting Slab Store Front"
-              width={1920}
-              height={1277}
-            />
-          </div>
-        </div>
+        
         <div className={styles.welcome}>
           <h2>
             Welcome to the Cutting Slab!
@@ -50,60 +45,62 @@ export default function Home() {
             {"We specialize in cutting every variety of bread. If we can't cut it, you shouldn't eat it."}
           </p>
           <p>Our world-class state-of-the-art advanced science technologies are able and willing to slice with accuracy and bravado.</p>
-        </div>   
-        <div className={styles.jackhead}>
-          <h2>See the Amazing Jumping Knife Jack!</h2>
-        </div>
-        <div>
-          <Image
+        </div>  
+
+
+        <div className={styles.jackContainer}>
+          <div className={styles.jackhead}>
+            <h2>See the Amazing Jumping Knife Jack!</h2>
+            <p>Broadway trained and ready for magic. You have never seen a more majestic knife.</p>
+          </div>
+          <img
             src="/images/jumpingknifejack.jpg"
             alt="The Amazing Jumping Knife Jack"
-            width={1282}
-            height={1920}
+            className={styles.jack}
           />
+          <div className={styles.afterjack}>
+          </div>
         </div>
-        <div className={styles.afterjack}>
-          <p>Broadway trained and ready for magic. You have never seen a more majestic knife.</p>
-        </div>
+
+
         <div className={styles.breads}>
-          <p>{"Don't you love bread?"}</p>
-          <Image
-            src="/images/bread1.jpg"
-            alt="Bread"
-            width={640}
-            height={399}
-          />
-          <p>{"Don't you love it sliced?"}</p>
-          <Image
-            src="/images/bread2.jpg"
-            alt="More Bread"
-            width={640}
-            height={426}
-          />
-          <p>{"It just looks so good doesn't it?"}</p>
-          <Image
-            src="/images/bread3.jpg"
-            alt="Yummy Bread"
-            width={640}
-            height={424}
-          />
-          <p>{"Oh you want that so bad don't you?"}</p>
-          <Image
-            src="/images/bread4.jpg"
-            alt="Lovely Bread"
-            width={640}
-            height={426}
-          />
-          <p>{"Come on over... We're waiting for you, big [boy or girl]"}</p>
+          <div className={styles.breadCard}>
+            <p>{"Don't you love bread?"}</p>
+            <img
+              src="/images/bread1.jpg"
+              alt="Bread"
+            />
+          </div>
+          <div className={styles.breadCard}>
+            <p>{"Don't you love it sliced?"}</p>
+            <img
+              src="/images/bread2.jpg"
+              alt="Bread"
+            />
+          </div>
+          <div className={styles.breadCard}>
+            <p>{"It just looks so good doesn't it?"}</p>
+            <img
+              src="/images/bread3.jpg"
+              alt="Bread"
+            />
+          </div>
+          <div className={styles.breadCard}>
+            <p>{"Oh you want that so bad don't you?"}</p>
+            <img
+              src="/images/bread4.jpg"
+              alt="Bread"
+            />
+          </div>
+          <div className={styles.afterBreads}>
+            <p>{"Come on over... We're waiting for you, big [boy or girl]"}</p>
+          </div>
         </div>
-        <div className="cuttingSlab">
-          <Image
-            src="/images/thecuttingslab.png"
-            alt="The Cutting Slab"
-            width={1129}
-            height={926}
-          />
-        </div>
+        <img
+          src="/images/thecuttingslab.png"
+          alt="The Cutting Slab"
+          className={styles.cuttingSlabBottom}
+        />
       </div>
      
       <style jsx global>{`
--- pages/index.js
+++ pages/index.js
@@ -7,28 +7,6 @@ export default function Home () {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-      
-        <div className={styles.left}>
-          <div className={styles.profileContainer}>
-            <img 
-              className={styles.profile}
-              alt="My Face"
-              width="200px"
-              src="/home/deepFriedLite.png"/>
-          </div>
-          <div className={styles.leftContent}>
-            <div className={styles.leftContent}>
-              <h2>About Me:</h2>
-              <p>My name is Joe Gonzalez.</p>
-              <p>I am a web developer based in Norfolk, Virginia.</p>
-              <p>I love working with JavaScript, HTML, CSS, React, Nodejs and much more!</p>
-              <p>I will be updating this site fequently with new content so check back regularly.</p>
-              <p>Please feel free to <a href="mailto:admin@eojhet.com"><strong>Contact Me</strong></a> for questions, comments, or if you are hiring.</p>
-              <p>Thank you so much for visiting!</p>
-            </div>
-          </div>
-        </div>
-
         <div className={styles.center}>
           <div className={styles.title}>
             <h1>Content...</h1>
@@ -43,17 +21,30 @@ export default function Home () {
                 src="/images/jumpingknifejack.jpg"
                 alt="The Amazing Jumping Knife Jack"
                 width={1282}
-                height={1920}
+                height={1400}
               />
             </div>
           </div></Link>
-         
         </div>
 
         <div className={styles.right}>
-          <h2>Thank you for visiting!</h2>
+          <div className={styles.profileContainer}>
+            <img 
+              className={styles.profile}
+              alt="My Face"
+              width="200px"
+              src="/home/deepFriedLite.png"/>
+          </div>
           <div className={styles.rightContent}>
-            Check the footer for my socials.
+            <div className={styles.rightContent}>
+              <h2>About Me:</h2>
+              <p>My name is Joe Gonzalez.</p>
+              <p>I am a web developer based in Norfolk, Virginia.</p>
+              <p>I love working with JavaScript, HTML, CSS, React, Nodejs and much more!</p>
+              <p>I will be updating this site fequently with new content so check back regularly.</p>
+              <p>Please feel free to <a href="mailto:admin@eojhet.com"><strong>Contact Me</strong></a> for questions, comments, or if you are hiring.</p>
+              <p>Thank you so much for visiting!</p>
+            </div>
           </div>
         </div>
         

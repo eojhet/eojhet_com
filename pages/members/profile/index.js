0b918341a50fe9a2d8ac1fd3bfbc6931422232aa
eojhet <eojhet@gmail.com>
2022-04-01 16:36:53 -0400

    profile page retrieves profile data on load

--- pages/members/profile/index.js
+++ pages/members/profile/index.js
@@ -1,15 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import GetToken from "../../../components/getToken";
 import styles from "./index.module.scss";
 
 export default function Profile () {
+  const [profile, setProfile] = useState({});
 
-  const token = GetToken();
+  useEffect(() => {
+    const profileData = async () => {
+
+      let token = 'Bearer ' + GetToken();
+
+      let data = await axios({
+        method: 'get',
+        url: 'http://localhost:8080/members/profile',
+        headers: {Authorization: token},
+      })
+      .then(function (res) {
+        return res.data
+      })
+      .catch(function (err) {
+        console.log('token ', token, 'err', err);
+      })
+
+      console.log('data', data);
+      return data;
+    }
+
+    setProfile(profileData);
+
+  }, [])
+
 
   return (
     <div className={styles.container}>
       <h1>Welcome</h1>
       <div className={styles.content}>
-        {token ? token : null}
       </div>
     </div>
   )

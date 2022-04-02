1ddad07f5079eb8b47754b02b8494d6dfcb135ec
Joe Gonzalez <eojhet@gmail.com>
2022-04-01 21:02:12 -0400

    profile shows displayName and joinDate

--- pages/members/profile/index.js
+++ pages/members/profile/index.js
@@ -6,35 +6,32 @@ import styles from "./index.module.scss";
 export default function Profile () {
   const [profile, setProfile] = useState({});
 
+  
   useEffect(() => {
-    const profileData = async () => {
-
-      let token = 'Bearer ' + GetToken();
-
-      let data = await axios({
-        method: 'get',
-        url: 'http://localhost:8080/members/profile',
-        headers: {Authorization: token},
-      })
-      .then(function (res) {
-        return res.data
-      })
-      .catch(function (err) {
-        console.log('token ', token, 'err', err);
-      })
-
-      console.log('data', data);
-      return data;
-    }
-
-    setProfile(profileData);
-
+    profileData();
   }, [])
 
+  async function profileData() {
+    let token = 'Bearer ' + GetToken();
+    let data = await axios({
+      method: 'get',
+      url: 'http://localhost:8080/members/profile',
+      headers: {Authorization: token},
+    })
+    .then(function (res) {
+      return res.data
+    })
+    .catch(function (err) {
+      console.log('token ', token, 'err', err);
+    })
+    console.log('data ', data);
+    setProfile(data);
+  }
 
   return (
     <div className={styles.container}>
-      <h1>Welcome</h1>
+      <h1>Welcome {profile.displayName}!</h1>
+      <p>Member since {new Date(profile.joinDate).toLocaleDateString('en-US', {month:'long', year:'numeric'})}.</p>
       <div className={styles.content}>
       </div>
     </div>

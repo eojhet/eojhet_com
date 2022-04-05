cf008080c814406977d8a699e616f718131630ad
eojhet <eojhet@gmail.com>
2022-04-05 16:04:22 -0400

    Redux now retrieves and stores profile data

--- components/redux/profileSlice.js
+++ components/redux/profileSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import GetToken from "../getToken";
 
 const initialState = {
   _id: undefined,
@@ -15,10 +17,37 @@ export const profileSlice = createSlice({
   name: 'profile',
   initialState,
   reducers: {
-
+    getProfile: (state, action) => {
+      state._id = action.payload._id;
+      state.joinDate = action.payload.joinDate;
+      state.displayName = action.payload.displayName;
+      state.firstName = action.payload.firstName;
+      state.lastName = action.payload.lastName;
+      state.bio = action.payload.bio;      
+    }
   }
 })
 
-export const selectProfile = (state) => state.profile.displayName;
+export const { getProfile } = profileSlice.actions;
+
+export const GetProfile = () => async (dispatch) => {
+  let token = 'Bearer ' + GetToken();
+  let data = await axios({
+    method: 'get',
+    url: '/api/profile',
+    headers: {Authorization: token},
+  })
+  .then(function (res) {
+    return res.data
+  })
+  .catch(function (err) {
+    console.log('token ', token, 'err', err);
+    return
+  })
+
+  dispatch(getProfile(data));
+}
+
+export const selectProfile = (state) => state.profile;
 
 export default profileSlice.reducer
--- pages/members/profile/index.js
+++ pages/members/profile/index.js
@@ -1,41 +1,33 @@
 // members/profile
 
-import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
-import { selectProfile } from "../../../components/redux/profileSlice";
-
-import GetProfile from "../../../components/getProfile";
+import { selectProfile, GetProfile } from "../../../components/redux/profileSlice";
 import styles from "./index.module.scss";
 
 export default function Profile () {
-  const [profile, setProfile] = useState({});
   const [editMode, setEditMode] = useState(false);
+
+  const dispatch = useDispatch();
   
-  // useEffect(() => {
-  //   getProfile();
-  // }, [])
+  useEffect(() => {
+    dispatch(GetProfile());
+  },[])
 
-  // async function getProfile () {
-  //   let profileData = await GetProfile()
-  //   console.log(profileData);
-  //   setProfile(profileData);
-  // }
-
-  const displayName = useSelector(selectProfile);
+  const reduxProfile = useSelector(selectProfile);
   
   function ProfileSection () {
     return (
       <div className={styles.container}>
         <div className={styles.heading}>
-          <h1>Welcome {profile.displayName}!</h1>
-          <p>Member since {new Date(profile.joinDate).toLocaleDateString('en-US', {month:'long', year:'numeric'})}.</p>
+          <h1>Welcome {reduxProfile.displayName}!</h1>
+          <p>Member since {new Date(reduxProfile.joinDate).toLocaleDateString('en-US', {month:'long', year:'numeric'})}.</p>
         </div>
         <div className={styles.content}>
-          <p>First Name: {profile.firstName}</p>
-          <p>Last Name: {profile.lastName}</p>
-          <p>Bio: {profile.bio}</p>
+          <p>First Name: {reduxProfile.firstName}</p>
+          <p>Last Name: {reduxProfile.lastName}</p>
+          <p>Bio: {reduxProfile.bio}</p>
           <button type='edit' onClick={() => setEditMode(!editMode)}>edit</button>
         </div> 
       </div>
@@ -46,14 +38,13 @@ export default function Profile () {
     return (
       <div>
         <h3>Please <Link href="/members/login">Log In</Link> to continue</h3>
-        <h3>{displayName}</h3>
       </div>
     )
   }
 
   return (
     <div className={styles.page}>
-      {profile._id ? <ProfileSection /> : <NoProfile />}
+      {reduxProfile._id ? <ProfileSection /> : <NoProfile />}
     </div>
   )
 }
